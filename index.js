665f6caf3e543c425780921cf2bc2876c3db6c40
Jan Raasch <jan@janraasch.com>
2015-08-28 19:57:43 +0200

    feat(humans.txt): pull and add humans.txt to names

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 "use strict";
 
 var messages = require("./commit_messages");
+var names = require("./humans");
 var lib = require("./lib");
-var names = [
-  "Nick", "Steve", "Andy", "Qi", "Fanny", "Sarah", "Cord", "Todd",
-  "Chris", "Pasha", "Gabe", "Tony", "Jason", "Randal", "Ali", "Kim",
-  "Rainer", "Guillaume", "Kelan", "David", "John", "Stephen", "Tom",
-  "Steven", "Jen", "Marcus", "Edy", "Rachel"
-];
 
 exports.whatThe = function () {
   var hash = lib.random_choice(Object.keys(messages));
--- scripts/pull.js
+++ scripts/pull.js
@@ -6,14 +6,12 @@ const editor = require('mem-fs-editor')
 const ent = require('ent')
 
 const commitMessagesTxt = 'https://raw.githubusercontent.com/ngerakines/commitment/master/commit_messages.txt'
-const template = (json) => `"use strict";\nmodule.exports = ${JSON.stringify(json, null, ' ')};\n`
+const humansTxt = 'https://raw.githubusercontent.com/ngerakines/commitment/master/static/humans.txt'
+const template = (json) => `'use strict';\nmodule.exports = ${JSON.stringify(json, null, ' ')};\n`
 
-const download = new Download({mode: '755'})
-    .get(commitMessagesTxt)
-    .dest('./downloads')
-
-const store = memFs.create()
-const fs = editor.create(store)
+// TODO: remove code dup (`downloadMessages` vs. `downloadHumans`).
+const downloadMessages = new Download({mode: '755'}).get(commitMessagesTxt).dest('./downloads')
+const downloadHumans = new Download({mode: '755'}).get(humansTxt).dest('./downloads')
 
 // Behaves like Python's `f.readlines()`.
 const readlines = (file) => file.contents.toString().split(/\r\n|\r|\n/g).map(line => `${line}\n`)
@@ -23,8 +21,10 @@ const hexdigest = (string) => crypto.createHash('md5').update(string).digest('he
 
 console.log('Updating ./commit_messages.js')
 
-download.run((err, files) => {
+downloadMessages.run((err, files) => {
     if (err || !files.length || !files[0]) { throw err }
+    const store = memFs.create()
+    const fs = editor.create(store)
     let json = {}
 
     readlines(files[0]).forEach(line => {
@@ -33,5 +33,28 @@ download.run((err, files) => {
     })
 
     fs.write('./commit_messages.js', template(json))
+    fs.commit(() => console.log('All done...'))
+})
+
+console.log('Updating ./humans.js')
+
+downloadHumans.run((err, files) => {
+    if (err || !files.length || !files[0]) { throw err }
+    const store = memFs.create()
+    const fs = editor.create(store)
+
+    let json = [
+      'Nick', 'Steve', 'Andy', 'Qi', 'Fanny', 'Sarah', 'Cord', 'Todd',
+      'Chris', 'Pasha', 'Gabe', 'Tony', 'Jason', 'Randal', 'Ali', 'Kim',
+      'Rainer', 'Guillaume', 'Kelan', 'David', 'John', 'Stephen', 'Tom',
+      'Steven', 'Jen', 'Marcus', 'Edy', 'Rachel'
+    ].concat(
+      files[0].contents.toString().split('\n')
+        .filter(line => line.indexOf('Name: ') === 0)
+        .map(line => line.substring(6))
+        .map(data => (data.indexOf('github:') === 0) ? data.substring(7) : data.split(' ')[0])
+    )
+
+    fs.write('./humans.js', template(json))
     fs.commit(() => console.log('All done...'))
 })
